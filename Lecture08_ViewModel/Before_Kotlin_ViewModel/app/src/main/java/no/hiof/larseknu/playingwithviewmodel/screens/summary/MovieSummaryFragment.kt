06df2d50bf661b8baaefa229dd58a80e4140b8c6
Lars emil <larseknu@hiof.no>
2019-09-16 13:04:14 +0200

    Lecture 08 - ViewModel example

---
+++ Lecture08_ViewModel/Before_Kotlin_ViewModel/app/src/main/java/no/hiof/larseknu/playingwithviewmodel/screens/summary/MovieSummaryFragment.kt
@@ -0,0 +1,42 @@
+package no.hiof.larseknu.playingwithviewmodel.screens.summary
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import no.hiof.larseknu.playingwithviewmodel.R
+import no.hiof.larseknu.playingwithviewmodel.databinding.FragmentMovieSummaryBinding
+import no.hiof.larseknu.playingwithviewmodel.model.Movie
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MovieSummaryFragment : Fragment() {
+    private lateinit var currentMovie : Movie
+
+    private var numberAnswered = 0
+    private var numberSeen = 0
+
+    private lateinit var movieList : MutableList<Movie>
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_summary, container, false)
+    }
+
+
+}
